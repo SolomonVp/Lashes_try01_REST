9ac0b685ae2e1b640e5ef5677655d06cdf58e680
SolomonVP <Lum7023@yandex.ru>
2022-11-06 03:37:41 +0300

    Oh my GOGH! This is my FIRST own request! OMG! OMG! OMG! Add getClientById method

--- src/main/java/com/lashes/spring/rest/controller/MyRESTController.java
+++ src/main/java/com/lashes/spring/rest/controller/MyRESTController.java
@@ -4,6 +4,7 @@ import com.lashes.spring.rest.entity.Client;
 import com.lashes.spring.rest.sevice.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,4 +24,11 @@ public class MyRESTController {
         return allClients;
 
     }
+
+    @GetMapping("clients/{id}")
+    public Client getClientById(@PathVariable int id) {
+        Client client = clientService.getClient(id);
+        return client;
+    }
+
 }
