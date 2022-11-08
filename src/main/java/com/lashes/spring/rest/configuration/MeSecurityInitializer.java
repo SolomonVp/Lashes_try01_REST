b601614d68dea5497105d5a50881248314315335
SolomonVP <Lum7023@yandex.ru>
2022-11-08 14:52:28 +0300

    добавление SpringSecurity configuration.

---
+++ src/main/java/com/lashes/spring/rest/configuration/MeSecurityInitializer.java
@@ -0,0 +1,6 @@
+package com.lashes.spring.rest.configuration;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class MeSecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+}
---
+++ src/main/java/com/lashes/spring/rest/configuration/MySecurityConfig.java
@@ -0,0 +1,17 @@
+package com.lashes.spring.rest.configuration;
+
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+
+public class MySecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        User.UserBuilder userBuilder = User.withDefaultPasswordEncoder();
+
+        auth.inMemoryAuthentication()
+                .withUser(userBuilder.username("zaur").password("zaur").roles("EMPLOYEE"))
+                .withUser(userBuilder.username("elena").password("elena").roles("HR"))
+                .withUser(userBuilder.username("ivan").password("ivan").roles("MANAGER", "HR"));
+    }
+}
